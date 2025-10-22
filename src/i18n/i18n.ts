845913b26f981216304c18d3b9787f93a53d462b
Dzmitry Melnikau <dzmitry.melnikau@innowise.com>
2025-10-22 13:34:54 +0200

    fix i18n structure

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -2,8 +2,8 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
-import enTranslation from '../translations/en.json';
-import arTranslation from '../translations/ar.json';
+import enTranslation from './en.json';
+import arTranslation from './ar.json';
 
 i18n
   // .use(Backend) // Temporarily disable Backend as we are importing translations directly
